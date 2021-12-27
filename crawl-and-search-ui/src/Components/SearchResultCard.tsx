c2fd05b25de41520b4c9eb85b8098abdd2af49ae
dario <darionunez1999@gmail.com>
2021-12-27 13:38:41 +0000

    Remove unused methods

--- crawl-and-search-ui/src/Components/SearchResultCard.tsx
+++ crawl-and-search-ui/src/Components/SearchResultCard.tsx
@@ -1,20 +1,13 @@
 import { Card, CardActions, CardContent, CardHeader, Chip, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 
-// For sure!
-// title
-// source
-
-// Maybe
-// Summary (200 line)
-// Tags
-
 interface ResultCardFields {
   url: string;
   title: string;
   summary: string;
   tags: Array<string>;
 }
+
 export function SearchResultCard(props: { content: ResultCardFields }) {
   return (
     <Box>
@@ -36,20 +29,4 @@ export function SearchResultCard(props: { content: ResultCardFields }) {
       </Card>
     </Box>
   )
-}
-
-
-function randomIntFromInterval(min: number, max: number) { // min and max included 
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-const generateRandomString = (size: number) => {
-  const characters = 'ABCD EFGH IJKL MNOP QRST UVWX YZab cdef ghij klmn opqr stuv wxyz 0123 4567 89';
-  let res = ' ';
-  const charactersLength = characters.length;
-  for (let i = 0; i < size; i++) {
-    res += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-
-  return res;
 }
