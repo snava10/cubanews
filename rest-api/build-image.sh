9e1b5326d692c61792a2d7b28690aa127b1f0982
Sergio Navarrete <sergio.nava89@gmail.com>
2022-01-16 23:15:59 +0000

    adding path to kustomization.yml

--- rest-api/build-image.sh
+++ rest-api/build-image.sh
@@ -1,2 +1,4 @@
-docker build -t cubanews-rest-api:1.0.0 \
-       --build-arg JAR_FILE=build/libs/rest-api-1.0.0-SNAPSHOT.jar .
+#docker build -t cubanews-rest-api:1.0.0 \
+#       --build-arg JAR_FILE=build/libs/rest-api-1.0.0-SNAPSHOT.jar .
+
+docker build --tag "gcr.io/$PROJECT_ID/cubanews-rest-api:v1" --build-arg JAR_FILE=build/libs/rest-api-1.0.0-SNAPSHOT.jar .
