eff9aca5d9424a451852ff106b21124cfc7d5155
Sergio Navarrete <sergio.nava89@gmail.com>
2022-03-29 00:21:34 +0100

    aligning search results to the left by adding padding

--- web-ui/src/Components/ResultStack.tsx
+++ web-ui/src/Components/ResultStack.tsx
@@ -4,7 +4,7 @@ import { SearchResultCard } from "./SearchResultCard";
 export function ResultStack(props: any) {
   // console.log("[Result stack] called")
   return (
-    <Stack spacing={1} sx={{ paddingTop: 2, paddingBottom: 2 }}>
+    <Stack spacing={1} sx={{ paddingTop: 2, paddingBottom: 2, paddingLeft: 1 }}>
       {props.searchResults.length === 1 && props.searchResults[0].title === 'No results' ? 
       // TODO: Make this a better UI.
       <p>No se encontraron resultados. Por favor modifique su criterio de busqueda e intente nuevamente.</p> :
