c1278482dd9b35ff22f78daa8a1ed1ea1ad2e493
Sergio Navarrete <sergio.nava89@gmail.com>
2022-12-11 21:55:23 +0000

    dev environment

--- rest-api/build-image.sh
+++ rest-api/build-image.sh
@@ -1,5 +1,5 @@
-#docker build -t cubanews-rest-api:1.0.0 \
-#       --build-arg JAR_FILE=build/libs/rest-api-1.0.0-SNAPSHOT.jar .
+docker build -t cubanews-rest-api:1.0.0 \
+       --build-arg JAR_FILE=build/libs/rest-api-1.0.0-SNAPSHOT.jar .
 
 # docker build --tag "gcr.io/$PROJECT_ID/cubanews-rest-api:v1" --build-arg JAR_FILE=build/libs/rest-api-1.0.0-SNAPSHOT.jar .
-docker build --tag "us-east1-docker.pkg.dev/$PROJECT_ID/cubanews/cubanews-rest-api:v1" --build-arg JAR_FILE=build/libs/rest-api-1.0.0-SNAPSHOT.jar .
+# docker build --tag "us-east1-docker.pkg.dev/$PROJECT_ID/cubanews/cubanews-rest-api:v1" --build-arg JAR_FILE=build/libs/rest-api-1.0.0-SNAPSHOT.jar .
