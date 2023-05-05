dffb3b26384083a5fb5b9670acf4a1d720d81078
Sergio Navarrete <sergio.nava89@gmail.com>
2023-05-05 23:54:31 +0100

    incorporating the GNews inpired card

--- web-ui/src/Components/SourceGroup.tsx
+++ web-ui/src/Components/SourceGroup.tsx
@@ -2,16 +2,13 @@ import Stack from "@mui/material/Stack";
 import { ResultCard } from "../Helpers/resultsGenerator";
 import { SearchResultCard } from "./SearchResultCard";
 import { Box } from "@mui/material";
+import { GNewsInspiredCard } from "./GNewsInspiredCard";
 
 export function SourceGroup(props: { source: string; cards: ResultCard[] }) {
   return (
     <Stack direction="row" spacing={1}>
       {props.cards.map((card: ResultCard) => (
-        <SearchResultCard
-          key={card.url}
-          content={card}
-          bucketName={props.source}
-        />
+        <GNewsInspiredCard content={card} />
       ))}
     </Stack>
   );
