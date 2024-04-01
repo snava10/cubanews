187994acfab6c6dfacc66ebd3d1c52fbde99300e
Sergio Navarrete <sergio.nava89@gmail.com>
2024-04-01 15:52:11 +0100

    Crawling home page of adn cuba

--- cubanewsjs/cubanews-crawler/src/main.ts
+++ cubanewsjs/cubanews-crawler/src/main.ts
@@ -1,13 +1,20 @@
-// For more information, see https://crawlee.dev/
+/**
+ * This is the ADN Cuba homepage crawler.
+ * It is aiming to crawl the home page of ADN Cuba and extract all the direct links to articles
+ * present in the page. Only one level.
+ * The articles are encapsulated in an <article> tab and contain a link inside that fits the pattern
+ * https://adncuba.com/asd/asdx
+ */
 import { PlaywrightCrawler } from "crawlee";
 import moment from "moment";
 import { Actor } from "apify";
 
 await Actor.init();
 
-const domainSubdomainTileRegex = RegExp(
-  `http[s]?:\/\/[\w.-]+\/[\w.-]+\/[\w.-]+`
-);
+const startPages = new Set([
+  "https://adncuba.com/",
+  "https://adncuba.com/noticias-de-cuba/",
+]);
 
 function isValid(url: string): boolean {
   const sections = url.split("/");
@@ -44,17 +51,21 @@ const crawler = new PlaywrightCrawler({
     }
     // Extract links from the current page
     // and add them to the crawling queue.
-    await enqueueLinks({
-      strategy: "same-hostname",
-    });
+    // Only enque links if we are in the start page. This is basically a depth=1 crawler.
+    if (request.loadedUrl && startPages.has(request.loadedUrl)) {
+      await enqueueLinks({
+        globs: ["http?(s)://adncuba.com/*/*"],
+        selector: "a",
+      });
+    }
   },
   // Comment this option to scrape the full website.
-  maxRequestsPerCrawl: 50,
+  // maxRequestsPerCrawl: 50,
   // Uncomment this option to see the browser window.
   // headless: false,
 });
 
-// Add first URL to the queue and start the crawl.
-await crawler.run(["https://adncuba.com/noticias-de-cuba"]);
+// Start on the ADN Cuba homepage.
+await crawler.run([...startPages]);
 
 await Actor.exit();
