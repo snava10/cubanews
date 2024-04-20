810288345d04b0e95644bf6ce42e8e53c8c6e604
Sergio Navarrete <sergio.nava89@gmail.com>
2024-04-20 16:22:31 +0100

    Setting up database and pulling the feed from. Naive implementation of creating the feed

--- cubanewsjs/cubanews-feed/pages/api/feed.ts
+++ cubanewsjs/cubanews-feed/pages/api/feed.ts
@@ -1,24 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { FeedResponseData, NewsFeed } from "../../interfaces";
+import { FeedResponseData, NewsFeed, NewsItem } from "../../interfaces";
+import { createKysely } from "@vercel/postgres-kysely";
+import { Database } from "./dataschema";
+
+const db = createKysely<Database>();
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<FeedResponseData>
 ) {
+  const feeds = await db.selectFrom("feed").selectAll().execute();
+
+  const items = feeds.map((f) => {
+    console.log(f.isodate);
+    const ni = {
+      title: f.title,
+      source: f.source,
+      url: f.url,
+      updated: f.updated,
+      isoDate: f.isodate.toString(),
+      feedts: f.feedts,
+    } as NewsItem;
+    return ni;
+  });
+
+  const timestamp = items.length > 0 ? items[0].feedts : 0;
+
   res.status(200).json({
     banter: "Cubanews feed!",
     content: {
-      timestamp: Date.now(),
-      feed: [
-        {
-          title: "Fake news 1",
-          url: "https://fn1.com",
-        },
-        {
-          title: "Fake news 2",
-          url: "https://fn2.com",
-        },
-      ],
+      timestamp,
+      feed: items,
     },
   });
 }
