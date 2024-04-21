931d71047e3d58f0bf886fa1bf3b5ce75ca69fd4
Sergio Navarrete <sergio.nava89@gmail.com>
2024-04-21 19:10:45 +0100

    Migrating to app routing. Improving the feed retrieval

---
+++ cubanewsjs/cubanews-feed/src/app/api/feed/route.ts
@@ -0,0 +1,124 @@
+import { FeedResponseData, NewsItem } from "../../interfaces";
+import { createKysely } from "@vercel/postgres-kysely";
+import { Database, FeedTable } from "../dataschema";
+import { NextRequest, NextResponse } from "next/server";
+import { ApifyClient } from "apify-client";
+import { sql } from "kysely";
+
+const db = createKysely<Database>();
+
+const client = new ApifyClient({
+  token: process.env.APIFY_TOKEN,
+});
+
+export async function GET(
+  request: NextRequest
+): Promise<NextResponse<FeedResponseData | null>> {
+  if (request.nextUrl.searchParams.get("refresh")) {
+    refreshFeed()
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+
+    return NextResponse.json(
+      {
+        banter: "Refreshing cubanews feed",
+      },
+      { status: 200 }
+    );
+  }
+  return getFeed();
+}
+
+async function getFeed(): Promise<NextResponse<FeedResponseData | null>> {
+  const latestFeedts = await db
+    .selectFrom("feed")
+    .select([sql`max(feed.feedts)`.as("feedts")])
+    .executeTakeFirst();
+
+  if (!latestFeedts?.feedts) {
+    return NextResponse.json(
+      {
+        banter: "No feeds available",
+      },
+      { status: 500 }
+    );
+  }
+
+  const latestFeedtsValue = latestFeedts.feedts as number;
+  const feeds = await db
+    .selectFrom("feed")
+    .selectAll()
+    .where("feed.feedts", "=", latestFeedtsValue)
+    .execute();
+
+  const items = feeds.map((f) => {
+    console.log(f.isodate);
+    const ni = {
+      title: f.title,
+      source: f.source,
+      url: f.url,
+      updated: f.updated,
+      isoDate: f.isodate.toString(),
+      feedts: f.feedts,
+    } as NewsItem;
+    return ni;
+  });
+
+  const timestamp = items.length > 0 ? items[0].feedts : 0;
+  return NextResponse.json(
+    {
+      banter: "Cubanews feed!",
+      content: {
+        timestamp,
+        feed: items,
+      },
+    },
+    { status: 200 }
+  );
+}
+
+async function refreshFeed(): Promise<NewsItem[]> {
+  const datasetCollectionClient = client.datasets();
+  const listDatasets = await datasetCollectionClient.list();
+  const dataset = listDatasets.items.find((dataset) =>
+    dataset.name?.startsWith("adncuba")
+  );
+  // If dataset found, return its ID
+  if (!dataset) {
+    return [];
+  }
+
+  const { items } = await client.dataset(dataset.id).listItems();
+
+  const newsItems = items
+    .map((item) => item as unknown)
+    .map((item) => item as NewsItem);
+
+  const currentDate = new Date();
+
+  const values = newsItems.map(
+    (x) => newsItemToFeedTable(x, currentDate) as any
+  );
+  const insertResult = await db
+    .insertInto("feed")
+    .values(values)
+    .executeTakeFirst();
+  console.log(insertResult.numInsertedOrUpdatedRows);
+  return newsItems;
+}
+
+function newsItemToFeedTable(ni: NewsItem, currentDate: Date): FeedTable {
+  const isoDateString = currentDate.toISOString();
+  const epochTimestamp = currentDate.getTime();
+
+  return {
+    content: "",
+    feedisodate: isoDateString,
+    feedts: epochTimestamp,
+    isodate: ni.isoDate,
+    source: ni.source,
+    title: ni.title,
+    updated: ni.updated,
+    url: ni.url,
+  } as FeedTable;
+}
---
+++ cubanewsjs/cubanews-feed/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Cubanews",
+  description: "Cubanews",
+  icons: [
+    {
+      rel: "icon",
+      type: "image/ico",
+      sizes: "32x32",
+      url: "assets/icon.ico",
+    },
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
