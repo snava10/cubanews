057815a5315031a9fb14b2ee00473d546ff8e35c
Sergio Navarrete <sergio.nava89@gmail.com>
2024-04-22 22:43:05 +0100

    cleaning up

--- cubanewsjs/cubanews-feed/src/app/components/navigation/index.tsx
+++ cubanewsjs/cubanews-feed/src/app/components/navigation/index.tsx
@@ -3,11 +3,6 @@ import { useState } from "react";
 import Navbar from "./navbar";
 
 const Navigation = () => {
-  // toggle sidebar
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
   return (
     <>
       <Navbar />
