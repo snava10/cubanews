2c82d036b17135e6cdab03c839160dfbb522cb6a
Sergio Navarrete <sergio.nava89@gmail.com>
2024-05-06 22:46:23 +0100

    parsing date

--- cubanewsjs/cubanews-crawler/src/catorceYMedioCrawler.ts
+++ cubanewsjs/cubanews-crawler/src/catorceYMedioCrawler.ts
@@ -1,8 +1,19 @@
 import { PlaywrightCrawlingContext, Dictionary } from "crawlee";
 import { CubanewsCrawler, saveData } from "./cubanewsCrawler.js";
 import { NewsSourceName, getNewsSourceByName } from "./crawlerUtils.js";
+import moment from "moment";
 
 const newsSource = getNewsSourceByName(NewsSourceName.CATORCEYMEDIO);
+
+function parseDate(rawDate: string): moment.Moment {
+  moment.locale("es");
+  return moment(rawDate, "DD [de] MMMM YYYY - HH:mm");
+}
+
+function isValid(url: string): boolean {
+  const sections = url.split("/");
+  return sections.length >= 5;
+}
 
 export default class CatorceYMedioCrawler extends CubanewsCrawler {
   constructor() {
@@ -14,23 +25,31 @@ export default class CatorceYMedioCrawler extends CubanewsCrawler {
   ): Promise<void> {
     const { page, request, enqueueLinks, log } = context;
     const title = await page.title();
-    log.info(`Title of ${request.loadedUrl} is '${title}'`);
-    log.info(`${title}`, { url: request.loadedUrl });
 
-    if (newsSource) {
-      await saveData(
-        {
-          title,
-          url: request.loadedUrl,
-          // updated: momentDate.unix(),
-          // isoDate: momentDate.toISOString(),
-          // content: content,
-        },
-        newsSource.datasetName,
-        process.env.NODE_ENV !== "dev"
-      );
+    if (request.loadedUrl && isValid(request.loadedUrl)) {
+      log.info(`Title of ${request.loadedUrl} is '${title}'`);
+      const rawDate = await page
+        .locator(".timestamp-atom")
+        .first()
+        .textContent();
+      console.log(rawDate);
+      if (rawDate) {
+        const momentDate = parseDate(rawDate);
+        if (newsSource) {
+          await saveData(
+            {
+              title,
+              url: request.loadedUrl,
+              updated: momentDate.unix(),
+              isoDate: momentDate.toISOString(),
+              // content: content,
+            },
+            newsSource.datasetName,
+            process.env.NODE_ENV !== "dev"
+          );
+        }
+      }
     }
-
     if (
       request.loadedUrl &&
       (newsSource.startUrls.has(request.loadedUrl) ||
