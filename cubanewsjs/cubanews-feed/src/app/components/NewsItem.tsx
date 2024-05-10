1c54c98eb2a6ea7f8d4d0c1bef504a6ed24ae37c
Dario Nunez <darionunez1999@gmail.com>
2024-05-10 16:26:54 +0000

    more work on the new style

--- cubanewsjs/cubanews-feed/src/app/components/NewsItem.tsx
+++ cubanewsjs/cubanews-feed/src/app/components/NewsItem.tsx
@@ -9,7 +9,9 @@ import {
   Chip,
   Divider,
   CardOverflow,
-  CardContent
+  CardContent,
+  AspectRatio,
+  Avatar
 } from "@mui/joy";
 import { NewsItem } from "../interfaces";
 import Image from "next/image";
@@ -31,7 +33,7 @@ export default function NewsItemComponent({ item }: NewsItemProps) {
   item.tags = ["tag 1", "tag 2", "tag 4", "tag 3"]
   return (
     <Stack spacing={4}>
-      <Card variant="outlined" sx={{ padding: 2 }}>
+      {/* <Card variant="outlined" sx={{ padding: 2 }}>
         <Stack direction="row" spacing={2}>
           <Box>
             <Image
@@ -76,51 +78,41 @@ export default function NewsItemComponent({ item }: NewsItemProps) {
             )}
           </Stack>
         </Stack>
-        {/* <Grid container>
-          <Grid item xs={2} md={1}>
-            <Box>
-              <Image
-                height={30}
-                width={30}
-                alt="Publication Logo"
-                src={"/source_logos/adncuba1.webp"}
-              />
-            </Box>
-          </Grid>
-          <Grid item xs={10} md={11}>
-            <Box>
-              <Typography>
-                <Link href={item.url}>{item.title}</Link>
-              </Typography>
-            </Box>
-          </Grid>
-        </Grid> */}
-      </Card>
+      </Card> */}
       <Card variant="outlined" sx={{ padding: 2 }}>
         <CardContent>
           <Link href={item.url}>
             <Typography level="h2" fontSize="xl">{item.title}</Typography>
           </Link>
-          <Typography level="body-sm">{item.content}</Typography>
+          <Typography level="body-sm">{item.content} ...</Typography>
         </CardContent>
         <CardOverflow variant="soft" sx={{ bgcolor: 'background.level1'}}>
           <Divider inset="context" />
           <CardContent orientation="horizontal">
+          <Stack direction="row" spacing={1} flexWrap="wrap" useFlexGap>
             <Typography level="body-sm" fontWeight="md" textColor="text.secondary">
               {moment(item.isoDate).fromNow()}
             </Typography>
-            <Divider orientation="vertical" />
+            <Divider orientation="vertical" sx={{ml:1, mr:1}}/>
+            <Box alignItems="center">
+              <Avatar 
+                size="sm"
+                alt="Publication Logo" src="/source_logos/adncuba1.webp" 
+                sx ={{"--Avatar-size": "23px"}}
+              />
+            </Box>
             <Typography level="body-sm" fontWeight="md" textColor="text.secondary">
               {item.source}
             </Typography>
-            <Divider orientation="vertical" />
+            <Divider orientation="vertical" sx={{ml:1, mr:1}}/>
               {item.tags.map((tagName: string) => (
-                <Chip variant="outlined">
+                <Chip variant="outlined" key={tagName}>
                   <Typography level="body-xs" fontWeight="lg" textColor="text.secondary">
                     {tagName}
                   </Typography>
                 </Chip>
               ))}
+            </Stack>
           </CardContent>
         </CardOverflow>
       </Card>
