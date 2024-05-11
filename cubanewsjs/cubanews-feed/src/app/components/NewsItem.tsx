10babb3b4016f9537de8a844da9920e7aa9e1f46
Dario Nunez <darionunez1999@gmail.com>
2024-05-11 19:38:35 +0000

    new source icon

--- cubanewsjs/cubanews-feed/src/app/components/NewsItem.tsx
+++ cubanewsjs/cubanews-feed/src/app/components/NewsItem.tsx
@@ -8,10 +8,12 @@ import {
   Divider,
   CardOverflow,
   CardContent,
-  Avatar
+  Avatar,
+  AspectRatio,
 } from "@mui/joy";
 import { NewsItem } from "../interfaces";
 import moment from "moment";
+import Image from "next/image";
 
 type NewsItemProps = {
   item: NewsItem;
@@ -23,36 +25,69 @@ export default function NewsItemComponent({ item }: NewsItemProps) {
       <Card variant="outlined" sx={{ padding: 2 }}>
         <CardContent>
           <Link href={item.url}>
-            <Typography level="h2" fontSize="xl">{item.title}</Typography>
+            <Typography level="h2" fontSize="xl">
+              {item.title}
+            </Typography>
           </Link>
           <Typography level="body-sm">{item.content} ...</Typography>
         </CardContent>
-        <CardOverflow variant="soft" sx={{ bgcolor: 'background.level1'}}>
+        <CardOverflow variant="soft" sx={{ bgcolor: "background.level1" }}>
           <Divider inset="context" />
           <CardContent orientation="horizontal">
-          <Stack direction="row" spacing={1} flexWrap="wrap" useFlexGap>
-            <Typography level="body-sm" fontWeight="md" textColor="text.secondary">
-              {moment(item.isoDate).fromNow()}
-            </Typography>
-            <Divider orientation="vertical" sx={{ml:1, mr:1}}/>
-            <Box>
-              <Avatar 
+            <Stack direction="row" spacing={1} flexWrap="wrap" useFlexGap>
+              <Typography
+                level="body-sm"
+                fontWeight="md"
+                textColor="text.secondary"
+              >
+                {moment(item.isoDate).fromNow()}
+              </Typography>
+              <Divider orientation="vertical" sx={{ ml: 1, mr: 1 }} />
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Image
+                  src={`cuban-flag.svg`}
+                  width={20}
+                  height={100}
+                  alt="Cuban FLag"
+                />
+
+                {/* <AspectRatio objectFit="contain">
+                <img
+                  src="/source_logos/adncuba1.webp"
+                />
+              </AspectRatio> */}
+
+                {/* <Avatar 
                 size="sm"
                 alt="Publication Logo" src="/source_logos/adncuba1.webp" 
                 sx ={{"--Avatar-size": "23px"}}
-              />
-            </Box>
-            <Typography level="body-sm" fontWeight="md" textColor="text.secondary">
-              {item.source}
-            </Typography>
-            <Divider orientation="vertical" sx={{ml:1, mr:1}}/>
-            {item.tags.map((tagName: string) => (
-              <Chip variant="outlined" key={tagName}>
-                <Typography level="body-xs" fontWeight="lg" textColor="text.secondary">
-                  {tagName}
-                </Typography>
-              </Chip>
-            ))}
+              /> */}
+              </Box>
+              <Typography
+                level="body-sm"
+                fontWeight="md"
+                textColor="text.secondary"
+              >
+                {item.source}
+              </Typography>
+              <Divider orientation="vertical" sx={{ ml: 1, mr: 1 }} />
+              {item.tags.map((tagName: string) => (
+                <Chip variant="outlined" key={tagName}>
+                  <Typography
+                    level="body-xs"
+                    fontWeight="lg"
+                    textColor="text.secondary"
+                  >
+                    {tagName}
+                  </Typography>
+                </Chip>
+              ))}
             </Stack>
           </CardContent>
         </CardOverflow>
