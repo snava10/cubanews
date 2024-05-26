544b8c5ff9db1c5dc728dce373be2c30751ec00a
Sergio Navarrete <sergio.nava89@gmail.com>
2024-05-26 15:21:56 +0100

    redirecting logs

--- cubanewsjs/cubanews-crawler/run.sh
+++ cubanewsjs/cubanews-crawler/run.sh
@@ -1,11 +1,11 @@
 # source /home/sergio/.bashrc;
 
 echo "Refreshing feed Locally"
-cd /home/sergio/github/cubanews/cubanewsjs/adncuba-crawler && npm run start;
-cd /home/sergio/github/cubanews/cubanewsjs/catorceYmedio-crawler && npm run start;
-cd /home/sergio/github/cubanews/cubanewsjs/cibercuba-crawler && npm run start;
-cd /home/sergio/github/cubanews/cubanewsjs/ddc-crawler && npm run start:local;
-cd /home/sergio/github/cubanews/cubanewsjs/cubanews-feed && npm run start:local;
+cd /home/sergio/github/cubanews/cubanewsjs/ddc-crawler && npm run start > ~/cubanews-crawler.log 2>&1;
+cd /home/sergio/github/cubanews/cubanewsjs/adncuba-crawler && npm run start > ~/cubanews-crawler.log 2>&1;
+cd /home/sergio/github/cubanews/cubanewsjs/catorceYmedio-crawler && npm run start > ~/cubanews-crawler.log 2>&1;
+cd /home/sergio/github/cubanews/cubanewsjs/cibercuba-crawler && npm run start > ~/cubanews-crawler.log 2>&1;
+cd /home/sergio/github/cubanews/cubanewsjs/cubanews-feed && npm run start:local > ~/cubanews-crawler.log 2>&1;
 
 # # node /home/sergio/github/cubanews/cubanewsjs/adncuba-crawler/dist/main.js;
 # cd /home/sergio/github/cubanews/cubanewsjs/catorceYmedio-crawler && npm run start;
