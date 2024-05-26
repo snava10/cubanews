cff9baa3a1e2ba4e2a3ff64b74ca688b9b4c883e
Sergio Navarrete <sergio.nava89@gmail.com>
2024-05-26 19:54:31 +0100

    catching error

--- cubanewsjs/cubanews-crawler/run.sh
+++ cubanewsjs/cubanews-crawler/run.sh
@@ -1,12 +1,12 @@
 # source /home/sergio/.bashrc;
 
-echo "Refreshing feed Locally"
+echo "$(date) Refreshing feed Locally" > ~/cubanews-crawler.log 2>&1;
 cd /home/sergio/github/cubanews/cubanewsjs/ddc-crawler && npm run start > ~/cubanews-crawler.log 2>&1;
 cd /home/sergio/github/cubanews/cubanewsjs/adncuba-crawler && npm run start > ~/cubanews-crawler.log 2>&1;
 cd /home/sergio/github/cubanews/cubanewsjs/catorceYmedio-crawler && npm run start > ~/cubanews-crawler.log 2>&1;
 cd /home/sergio/github/cubanews/cubanewsjs/cibercuba-crawler && npm run start > ~/cubanews-crawler.log 2>&1;
 cd /home/sergio/github/cubanews/cubanewsjs/cubanews-feed && npm run start:local > ~/cubanews-crawler.log 2>&1;
-
+echo "$(date) Comleted Refreshing feed Locally" > ~/cubanews-crawler.log 2>&1;
 # # node /home/sergio/github/cubanews/cubanewsjs/adncuba-crawler/dist/main.js;
 # cd /home/sergio/github/cubanews/cubanewsjs/catorceYmedio-crawler && npm run start;
 # cd /home/sergio/github/cubanews/cubanewsjs/cibercuba-crawler && npm run start;
