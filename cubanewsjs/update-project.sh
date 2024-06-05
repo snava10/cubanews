d7cdeddba526afb8faaf01fe9da7832f2cb50ddd
Sergio Navarrete <sergio.nava89@gmail.com>
2024-06-05 13:11:30 +0100

    creating script to update project. Needed to deply changes to EC2

---
+++ cubanewsjs/update-project.sh
@@ -0,0 +1,23 @@
+echo "Pulling latest from main";
+git pull;
+
+echo "Building cubanews crawler";
+cd cubanews-crawler; npm i; npm run build;
+
+echo "Updating adncuba-crawler";
+cd ../adncuba-crawler; npm i; npm link ../cubanews-crawler;
+
+echo "Updating caterceYmedio-crawler";
+cd ../catorceYmedio-crawler; npm i; npm link ../cubanews-crawler;
+
+echo "Updating cibercuba-crawler";
+cd ../cibercuba-crawler; npm i; npm link ../cubanews-crawler;
+
+echo "Updating ddc-crawler";
+cd ../ddc-crawler; npm i; npm link ../cubanews-crawler;
+
+echo "Updating eltoque-crawler";
+cd ../eltoque-crawler; npm i; npm link ../cubanews-crawler;
+
+echo "Updating cubanet-crawler"
+cd ../cubanet-crawler; npm i; npm link ../cubanews-crawler;
