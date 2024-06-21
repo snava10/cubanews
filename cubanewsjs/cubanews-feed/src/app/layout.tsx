435ed485ce01befbebeec625868bdad6a1d9b8ba
Sergio Navarrete <sergio.nava89@gmail.com>
2024-06-21 20:13:36 +0100

    Speed insights

--- cubanewsjs/cubanews-feed/src/app/layout.tsx
+++ cubanewsjs/cubanews-feed/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/navigation";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
         <Navigation />
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
