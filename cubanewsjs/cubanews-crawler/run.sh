f4a408f2856860a2ae7b2ab6bc4c66bc4aa8d926
Sergio Navarrete <sergio.nava89@gmail.com>
2024-06-23 00:32:52 +0100

    run mailing list

--- cubanewsjs/cubanews-crawler/run.sh
+++ cubanewsjs/cubanews-crawler/run.sh
@@ -6,5 +6,6 @@ cd $HOME/github/cubanews/cubanewsjs/catorceYmedio-crawler && npm run start > $HO
 cd $HOME/github/cubanews/cubanewsjs/cibercuba-crawler && npm run start > $HOME/cubanews-crawler.log 2>&1;
 cd $HOME/github/cubanews/cubanewsjs/cubanet-crawler && npm run start > $HOME/cubanews-crawler.log 2>&1;
 cd $HOME/github/cubanews/cubanewsjs/cubanews-feed && npm run start:local > $HOME/cubanews-crawler.log 2>&1;
+npm run start:mail > $HOME/cubanews-crawler.log 2>&1;
 
-echo "$(date) Comleted Refreshing feed Locally" > ~/cubanews-crawler.log 2>&1;
+echo "$(date) Completed Refreshing feed Locally" > ~/cubanews-crawler.log 2>&1;
